cd3e59cc865aa83eb9d9f09c8ad2a2ab2ad13fc5
claneo <claneo@outlook.com>
2019-05-05 09:23:10 +0800

    index入口

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const fromPackageJson = require('./fromPackageJson');
 const fromPackageLockJson = require('./fromPackageLockJson');
 const fromTop = require('./fromTop');
 
-// fromInput(['cross-spawn@^6.0.5', 'semver@^4.0.0']);
-fromTop(1000);
-// fromPackageJson(path.resolve(__dirname, './package.json'));
-// fromPackageLockJson(path.resolve(__dirname, '../annotator/package-lock.json'));
+switch (process.argv[2]) {
+  case 'json':
+    if (process.argv[3]) {
+      fromPackageJson(path.resolve(process.argv[3], 'package.json'));
+    }
+    break;
+  case 'lock':
+    if (process.argv[3]) {
+      fromPackageLockJson(path.resolve(process.argv[3], 'package-lock.json'));
+    }
+    break;
+  case 'name':
+    fromInput(process.argv.slice(3));
+    break;
+  case 'top':
+    fromTop(process.argv[3]);
+    break;
+  default:
+    console.log('err');
+    break;
+}
